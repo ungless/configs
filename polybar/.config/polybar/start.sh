b7b67f52f94aa3a7e10ee095cf23a387972b9f7b
Max Ungless <maxungless@gmail.com>
2017-12-09 11:50:31 +0000

    [polybar] Added polybar module

--- polybar/.config/polybar/start.sh
+++ polybar/.config/polybar/start.sh
@@ -6,4 +6,5 @@ killall -q polybar
 # Wait until the processes have been shut down
 while pgrep -x polybar >/dev/null; do sleep 1; done
 
-polybar -r main &
+MONITOR=DisplayPort-0 polybar -r main &
+MONITOR=DVI-0 polybar -r main &
