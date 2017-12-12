498d822dbd8541db83b0473a082716a49383140e
Max Ungless <maxungless@gmail.com>
2017-12-12 19:22:08 +0000

    [polybar] Improved load time, added day and changed colours

--- polybar/.config/polybar/start.sh
+++ polybar/.config/polybar/start.sh
@@ -4,7 +4,5 @@
 killall -q polybar
 
 # Wait until the processes have been shut down
-while pgrep -x polybar >/dev/null; do sleep 1; done
-
 MONITOR=DisplayPort-0 polybar -r main &
 MONITOR=DVI-0 polybar -r main &
