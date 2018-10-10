668338aaab403021e947349cab24a741825d742e
Max Ungless <maxungless@gmail.com>
2018-10-10 18:22:11 +0100

    [cronjobs] Updated for speed

--- cronjobs/.cronjobs/syncbackup.sh
+++ cronjobs/.cronjobs/syncbackup.sh
@@ -5,16 +5,7 @@ echo "-------------- **** Starting Workspace Backup $(date) **** -------------"
 TEMP_FILE=`mktemp --suffix .workspacebackup`
 touch $TEMP_FILE
 
-rsync --progress -a -r /home/max/workspace/ /mnt/data/Linux/workspace
-
-rm $TEMP_FILE
-
-echo "-------------- **** Starting Internal Drive Backup $(date) **** -------------"
-
-TEMP_FILE=`mktemp --suffix .internalbackup`
-touch $TEMP_FILE
-
-rsync --progress -a -r /mnt/data/ /mnt/backup/
+rsync --progress -ahr /home/max/workspace/ /mnt/data/Linux/workspace
 
 rm $TEMP_FILE
 
@@ -23,7 +14,7 @@ echo "-------------- **** Starting Configs Backup $(date) **** -------------"
 TEMP_FILE=`mktemp --suffix .configsbackup`
 touch $TEMP_FILE
 
-rsync --progress -a -r /home/max/configs/ /mnt/data/Linux/configs
+rsync --progress -ahr /home/max/configs/ /mnt/data/Linux/configs
 
 rm $TEMP_FILE
 
@@ -32,9 +23,9 @@ echo "-------------- **** Starting General Folders Backup $(date) **** ---------
 TEMP_FILE=`mktemp --suffix .generalbackup`
 touch $TEMP_FILE
 
-rsync --progress -a -r /home/max/Books /mnt/data/Linux
-rsync --progress -a -r /home/max/Documents /mnt/data/Linux
-rsync --progress -a -r /home/max/Pictures /mnt/data/Linux
+rsync --progress -ahr /home/max/Books /mnt/data/Linux
+rsync --progress -ahr /home/max/Documents /mnt/data/Linux
+rsync --progress -ahr /home/max/Pictures /mnt/data/Linux
 
 
 rm $TEMP_FILE
@@ -44,7 +35,16 @@ echo "-------------- **** Starting Music Backup $(date) **** -------------"
 TEMP_FILE=`mktemp --suffix .musicbackup`
 touch $TEMP_FILE
 
-rsync --progress -a -r /home/max/Music /mnt/data/Linux
+rsync --progress -ahr /home/max/Music /mnt/data/Linux
+
+rm $TEMP_FILE
+
+echo "-------------- **** Starting Internal Drive Backup $(date) **** -------------"
+
+TEMP_FILE=`mktemp --suffix .internalbackup`
+touch $TEMP_FILE
+
+rsync --progress -ahr /mnt/data/ /mnt/backup/
 
 rm $TEMP_FILE
 
