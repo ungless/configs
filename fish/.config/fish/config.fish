3d2ff6df85c1e7e1a734fe5cd4916300b36ac073
Max Ungless <maxungless@gmail.com>
2018-10-10 18:22:28 +0100

    [fish] Added EDITOR

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -15,12 +15,19 @@ set __fish_git_prompt_char_upstream_ahead '↑ '
 set __fish_git_prompt_char_upstream_behind '↓ '
 
 # env variables
-set -gx EDITOR emacsclient -c
+set -gx EDITOR "emacsclient -c"
 set -gx WORKSPACE $HOME/workspace
 set -gx GOPATH $WORKSPACE/programming/go
-set -g GOBIN $GOPATH/bin
+set -gx GOBIN $GOPATH/bin
 set -gx PATH $HOME/.gem/ruby/2.4.0/bin $GOBIN $PATH
 
 function fish_prompt
     printf '%s@%s %s%s%s%s> ' (whoami) (hostname) (set_color $fish_color_cwd) (prompt_pwd) (set_color normal) (__fish_git_prompt)
+end
+
+# Start X at login
+if status is-login
+    if test -z "$DISPLAY" -a $XDG_VTNR = 1
+        exec startx -- -keeptty
+    end
 end
