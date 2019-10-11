02f30edc1d70c695d3745097ade11943a272f0a4
Max Ungless <maxungless@gmail.com>
2019-10-11 19:36:24 +0100

    [i3] update

--- i3/.config/i3/scripts/exam_dates.sh
+++ i3/.config/i3/scripts/exam_dates.sh
@@ -1,8 +1,18 @@
 #!/bin/bash
 
-echo "Latin Language:" $(( ($(date +%s -ud '2018-05-14 00:00:00') - $(date +%s)) / 3600 / 24 )) "days to go"
-echo "Latin Prose:" $(( ($(date +%s -ud '2018-05-17 00:00:00') - $(date +%s)) / 3600 / 24 )) "days to go"
-echo "Latin Verse:" $(( ($(date +%s -ud '2018-05-21 00:00:00') - $(date +%s)) / 3600 / 24 )) "days to go"
-echo "English:" $(( ($(date +%s -ud '2018-06-05 00:00:00') - $(date +%s)) / 3600 / 24 )) "days to go"
-echo "French:" $(( ($(date +%s -ud '2018-05-15 00:00:00') - $(date +%s)) / 3600 / 24 )) "days to go"
-echo "Dance:" $(( ($(date +%s -ud '2018-06-19 00:00:00') - $(date +%s)) / 3600 / 24 )) "days to go"
+echo "RE Paper 1:" $(( ($(date +%s -ud '2019-05-13 00:00:00') - $(date +%s)) / 3600 / 24 )) "days to go"
+echo "RE Paper 2:" $(( ($(date +%s -ud '2019-05-20 00:00:00') - $(date +%s)) / 3600 / 24 )) "days to go"
+echo "Biology Paper 1:" $(( ($(date +%s -ud '2019-05-14 00:00:00') - $(date +%s)) / 3600 / 24 )) "days to go"
+echo "Biology Paper 2:" $(( ($(date +%s -ud '2019-06-07 00:00:00') - $(date +%s)) / 3600 / 24 )) "days to go"
+echo "English Paper 1:" $(( ($(date +%s -ud '2019-05-15 00:00:00') - $(date +%s)) / 3600 / 24 )) "days to go"
+echo "English Paper 2:" $(( ($(date +%s -ud '2019-05-23 00:00:00') - $(date +%s)) / 3600 / 24 )) "days to go"
+echo "Chemistry Paper 1:" $(( ($(date +%s -ud '2019-05-16 00:00:00') - $(date +%s)) / 3600 / 24 )) "days to go"
+echo "Chemistry Paper 2:" $(( ($(date +%s -ud '2019-06-12 00:00:00') - $(date +%s)) / 3600 / 24 )) "days to go"
+echo "Drama:" $(( ($(date +%s -ud '2019-05-17 00:00:00') - $(date +%s)) / 3600 / 24 )) "days to go"
+echo "Mathematics Paper 1:" $(( ($(date +%s -ud '2019-05-21 00:00:00') - $(date +%s)) / 3600 / 24 )) "days to go"
+echo "Mathematics Paper 2:" $(( ($(date +%s -ud '2019-06-06 00:00:00') - $(date +%s)) / 3600 / 24 )) "days to go"
+echo "Mathematics Paper 3:" $(( ($(date +%s -ud '2019-06-11 00:00:00') - $(date +%s)) / 3600 / 24 )) "days to go"
+echo "Physics Paper 1:" $(( ($(date +%s -ud '2019-05-22 00:00:00') - $(date +%s)) / 3600 / 24 )) "days to go"
+echo "Physics Paper 2:" $(( ($(date +%s -ud '2019-06-14 00:00:00') - $(date +%s)) / 3600 / 24 )) "days to go"
+
+
