57f6981764f960049875525ba0369e75d24fd5a3
Max Ungless <maxungless@gmail.com>
2020-07-22 21:19:26 +0100

    [dwm] updated shutdown menu to lock

--- dwm/.dwm/shutdownmenu.sh
+++ dwm/.dwm/shutdownmenu.sh
@@ -1,14 +1,20 @@
-#! /bin/bash
-actions=("restart\nshutdown\nsleep\nhibernate")
+#!/bin/sh
+
+actions=("lock\nrestart\nshutdown\nsleep\nhibernate")
+
 action=$(echo -e $actions | dmenu -fn "Mononoki:size=11")
+
 case "$action" in
+    lock)
+        xscreensaver-command -lock
+        ;;
+
     restart)
         systemctl reboot
         ;;
 
     shutdown)
         systemctl poweroff
-        
         ;;
 
     sleep)
